1ef5ee4a6a5bcf65cf6737c92d8ad15d47099295
Kowen2206 <rafael.anaya@inmersys.com>
2023-12-20 15:09:24 -0600

    quiz logic

---
+++ Assets/_Project/Scripts/ScriptableObjects/QuizData.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newQuiz", menuName = "CreateQuiz")]
+public class QuizData : ScriptableObject
+{
+
+    [Serializable]
+    public struct TrueFalseQuestion
+    {
+        public string Question;
+        public bool Answer;
+    }
+
+    [Serializable]
+    public struct MultipleChoiceQuestion
+    {
+        public string Question;
+        public string Answer;
+        public List<string> options;
+    }
+
+    [SerializeField] private List<MultipleChoiceQuestion> _multipleChoiceQuestions;
+    [SerializeField] private List<TrueFalseQuestion> _trueFalseQuestions;
+
+    public List<MultipleChoiceQuestion> MultipleChoiceQuestions => _multipleChoiceQuestions;
+    public List<TrueFalseQuestion> TrueFalseQuestions => _trueFalseQuestions;
+}
---
+++ Assets/_Project/Scripts/UI/Quiz.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using TMPro.EditorUtilities;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quiz : MonoBehaviour
+{
+    private int _questionIndex;
+    [SerializeField] private TextMeshProUGUI _multipleChoiceQuestionText;
+    [SerializeField] private TextMeshProUGUI _trueFalseQuestionText;
+    [SerializeField] private TextMeshProUGUI _results;
+
+    [SerializeField] private TMP_Dropdown _dropDown;
+    [SerializeField] private QuizData _quiz;
+
+    [SerializeField] private GameObject _drowpDownPanel;
+    [SerializeField] private GameObject _trueFalsePanel;
+    [SerializeField] private GameObject _ResultsPanel;
+    private int _rightAnswers;
+    
+    private void StartMultipleChoiceQuiz()
+    {
+        _drowpDownPanel.SetActive(true);
+        _dropDown.value = 0;
+        _questionIndex = 0;
+        ShowMultipleChoiceQuestion();
+        
+    }
+
+    private void ShowMultipleChoiceQuestion()
+    {
+        _multipleChoiceQuestionText.text = _quiz.MultipleChoiceQuestions[_questionIndex].Question;
+        _dropDown.ClearOptions();
+        _dropDown.AddOptions(_quiz.MultipleChoiceQuestions[_questionIndex].options);
+    }
+
+    public void CheckDropDownAnswer()
+    {
+        if(_dropDown.options[_dropDown.value].text == _quiz.MultipleChoiceQuestions[_questionIndex].Answer)
+        _rightAnswers++;
+    }
+
+    public void ContinueDropDownQuiz()
+    {
+        if(_questionIndex < _quiz.MultipleChoiceQuestions.Count)
+        {
+            _questionIndex++;
+            ShowMultipleChoiceQuestion();
+        }
+        else
+        {
+            _drowpDownPanel.SetActive(false);
+            StartTrueFalseQuestions();
+        }
+    }
+
+    public void StartTrueFalseQuestions()
+    {
+        _trueFalsePanel.SetActive(true);
+        _questionIndex = 0;
+
+    }
+
+    public void ShowTrueFalseQuestion()
+    {
+        _trueFalseQuestionText.text = _quiz.TrueFalseQuestions[_questionIndex].Question;
+    }
+
+    public void CheckTrueFalseQuestion(bool answer)
+    {
+        if(_quiz.TrueFalseQuestions[_questionIndex].Answer == answer)
+        {
+            _rightAnswers++;
+        }
+
+        if(_questionIndex >= _quiz.TrueFalseQuestions.Count)
+        {
+            
+        }
+    }
+
+    public void ContinueTrueFalseQuiz()
+    {
+        if(_questionIndex < _quiz.MultipleChoiceQuestions.Count)
+        {
+            _questionIndex++;
+            ShowTrueFalseQuestion();
+        }
+        else
+        {
+            _trueFalsePanel.SetActive(false);
+            ShowResults();
+        }
+    }
+
+    public void ShowResults()
+    {
+        _ResultsPanel.SetActive(true);
+        _results.text = $"{_rightAnswers}/{_quiz.TrueFalseQuestions.Count + _quiz.MultipleChoiceQuestions.Count}";
+    }
+}
