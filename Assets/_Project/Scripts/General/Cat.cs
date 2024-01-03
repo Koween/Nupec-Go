6e0fa87d5b0c6d06647195f93b8d84da29d7304c
Koween <rafael.anaya@inmersys.com>
2024-01-03 12:16:36 -0600

    changed replaced animation events for getAnimDuration method

--- Assets/_Project/Scripts/General/Cat.cs
+++ Assets/_Project/Scripts/General/Cat.cs
@@ -6,10 +6,34 @@ using UnityEngine.Events;
 public class Cat : MonoBehaviour
 {
     [SerializeField] private UnityEvent _onFinishAnimation;
-
-
-    public void OnFinishAnimation()
+    [SerializeField] private Animator _catAnimator;
+    
+    private float GetAnimationDuration(string animationName)
     {
+        float length = -1;
+        AnimationClip[] clips = _catAnimator.runtimeAnimatorController.animationClips;
+        foreach(AnimationClip clip in clips)
+        {
+            if(clip.name == animationName)
+            {
+                length = clip.length;
+                break;
+            }
+        }
+        return  length;
+    }
+
+    public void WaitForFinishAnimation(string animationName)
+    {
+        StopAllCoroutines();
+        StartCoroutine(AnimationTimer(animationName));
+    }
+
+    public IEnumerator AnimationTimer(string animationName)
+    {
+        Debug.Log("animation length");
+        Debug.Log(GetAnimationDuration(animationName));
+        yield return new WaitForSeconds(GetAnimationDuration(animationName));
         _onFinishAnimation?.Invoke();
     }
 
